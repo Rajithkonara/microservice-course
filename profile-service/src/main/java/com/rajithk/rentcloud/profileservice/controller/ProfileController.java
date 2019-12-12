f0a28b8d8e717f1c6d8bb34583603ede3d5bbb4a
Rajithkonara <rajithk@wso2.com>
2019-12-12 15:47:04 +0530

    all added

--- profile-service/src/main/java/com/rajithk/rentcloud/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/rajithk/rentcloud/profileservice/controller/ProfileController.java
@@ -4,6 +4,7 @@ import com.rajithk.rentcloud.model.Customer;
 import com.rajithk.rentcloud.profileservice.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class ProfileController {
      }
 
      @PostMapping(value = "/profile")
+     @PreAuthorize(value = "hasAnyAuthority('create_profile')")
      public Customer save(@RequestBody Customer customer) {
           return customerService.save(customer);
      }
--- rentcloud/src/main/java/com/rajithk/rentcloud/model/Customer.java
+++ rentcloud/src/main/java/com/rajithk/rentcloud/model/Customer.java
@@ -3,10 +3,7 @@ package com.rajithk.rentcloud.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "customer")
@@ -14,7 +11,7 @@ import javax.persistence.Table;
 public class Customer {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private int id;
 
     private String firstName;
